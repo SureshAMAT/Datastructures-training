dcf5becaa7e425cedc6908d2d4798bb7238f776d
SureshAMAT <sureshrajendran24@gmail.com>
2021-08-23 23:10:28 +0530

    Singly Linked LIst

--- Source.cpp
+++ Source.cpp
@@ -1,39 +1,33 @@
-
 #include<iostream>
 using namespace std;
-void leftRotatebyOne(int arr[], int n)
-{
-	int temp = arr[0], i;
-	for (i = 0; i < n - 1; i++)
-		arr[i] = arr[i + 1];
-
-	arr[n - 1] = temp;
-}
-
-void leftRotate(int arr[], int d, int n)
-{
-	for (int i = 0; i < d; i++)
-		leftRotatebyOne(arr, n);
-}
-
-void printArray(int arr[], int n)
-{
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-}
-
-int main()
-{
-	int d;
-	cout << "enter no.of rotations";
-	cin >> d;
-	int arr[7];
-	cout << "enter array elements";
-	for (int i = 0; i < 7; i++) {
-		cin>>arr[i];
+class node {
+public:
+	int data;
+	node *next;
+};
+void print(node *n) {
+	while (n != NULL) {
+		cout << n->data << " ";
+		n = n->next;
 	}
-	int n = sizeof(arr);
-	leftRotate(arr, d, n);
-	printArray(arr, n);
-	return 0;
+}
+int main() {
+	node *head = NULL;
+	node *second = NULL;
+	node *third = NULL;
+
+	head = new node();
+	second = new node();
+	third = new node();
+
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third;
+	third->data = 3;
+	third->next = NULL;
+	print(head);
+	int sample;
+	cin >> sample;
 }
